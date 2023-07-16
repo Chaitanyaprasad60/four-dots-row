2555cfbddabbea416841bd1705a7eeed67b5795c
= <chaitanyaprasad60@gmail.com>
2023-07-16 13:05:14 +0530

    CG: Added url on fe

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl:"https://four-dots-row-git4-chaitanya60-dev.apps.sandbox-m3.1530.p1.openshiftapps.com/",
+  backendUrl:"https://four-dots-row-git10-chaitanya60-dev.apps.sandbox-m3.1530.p1.openshiftapps.com/",
   uiUrl:"https://chaitanyaprasad60.github.io/four-dots-row/#/"
 };
